633aa21611b5f1b8728ad0ffa5ecd1b56559bc53
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-04-20 15:41:21 +0200

    r

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+const localStorageKey = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(function(data) {
+  localStorage.setItem(localStorageKey, JSON.stringify(data.seconds));
+}, 1000));
+
+const savedTime = localStorage.getItem(localStorageKey);
+if (savedTime) {
+  player.setCurrentTime(JSON.parse(savedTime));
+}
+

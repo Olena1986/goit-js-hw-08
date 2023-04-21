f7222b966e3eb08267677edb9cace85fc54e0e3d
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-04-21 08:23:03 +0200

    о

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,19 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
     const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
+
+  
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
 
     player.on('timeupdate', throttle(function(event) {
     const time = Math.round(event.seconds);
     localStorage.setItem('videoplayer-current-time', time.toString());
 }, 1000));
 
-const currentTime = localStorage.getItem('videoplayer-current-time');
+    const currentTime = localStorage.getItem('videoplayer-current-time');
      
      if (currentTime) {
      player.setCurrentTime(parseInt(currentTime));
